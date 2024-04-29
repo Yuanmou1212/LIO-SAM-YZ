d26899bd19fa43ff0d9086f9844ce955b7e1528a
Yuanmou1212 <y.zhang4@student.tue.nl>
2024-04-29 23:18:10 +0800

    ;

--- include/utility.h
+++ include/utility.h
@@ -267,7 +267,7 @@ public:
         // rotate roll pitch yaw
         // Eigen::Quaterniond q_from(imu_in.orientation.w, imu_in.orientation.x, imu_in.orientation.y, imu_in.orientation.z);
         // Eigen::Quaterniond q_final = q_from * extQRPY;
-        Eigen::Quaterniond q_final = extQRPY
+        Eigen::Quaterniond q_final = extQRPY;
         imu_out.orientation.x = q_final.x();
         imu_out.orientation.y = q_final.y();
         imu_out.orientation.z = q_final.z();
